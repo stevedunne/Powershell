0eee7e1fff33fada32b09159612b52eeb001d8ba
Stephen Dunne <stephen_dunne@dell.com>
2015-10-16 13:46:03 -0500

    Adding VSVars and the basic profile script

---
+++ Profile.ps1
@@ -0,0 +1,12 @@
+﻿
+#execute this file from your profile (ise and regular) to load the various functions
+#and helpers into your powershell session
+
+. C:\Code\Powershell\VsVars.ps1
+
+#add Git to the env path
+$gitInstallDir = "C:\Program Files (x86)\Git\bin"
+if(!$env:path.Contains($gitInstallDir) ){
+    $env:path = $env:path + ";$gitInstallDir"
+    Write-Host "Added GIT to env path"
+}
---
+++ VsVars.ps1
@@ -0,0 +1,44 @@
+﻿
+#from http://www.hanselman.com/blog/AwesomeVisualStudioCommandPromptAndPowerShellIconsWithOverlays.aspx
+
+function Get-Batchfile ($file) {
+    $cmd = "`"$file`" & set"
+    cmd /c $cmd | Foreach-Object {
+        $p, $v = $_.split('=')
+
+    #    if( (get-item -path env:$p).Value -eq $v){
+    #        write-host "$p is the same" 
+    #    }
+    #    else{
+
+    #        write-host "Setting $p to $v"
+            Set-Item -path env:$p -value $v
+    #    }
+    }
+
+#    $commandList = @("INCLUDE", "PATH", "")
+}
+  
+#function VsVars32($version = "10.0")
+function VsVars()
+{
+ #   $key = "HKLM:SOFTWARE\Microsoft\VisualStudio\" + $version
+ #   $VsKey = get-ItemProperty $key
+
+ #   $VsInstallPath = [System.IO.Path]::GetDirectoryName($VsKey.InstallDir)
+ #   $VsToolsDir = [System.IO.Path]::GetDirectoryName($VsInstallPath)
+ #   $VsToolsDir = [System.IO.Path]::Combine($VsToolsDir, "Tools")
+ #   $BatchFile = [System.IO.Path]::Combine($VsToolsDir, "vsvars32.bat")
+
+ #   Get-Batchfile $BatchFile
+
+    Get-Batchfile "C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\Tools\vsvars32.bat"
+
+ #  [System.Console]::Title = "Visual Studio " + $version + " Windows Powershell"
+ #   //add a call to set-consoleicon as seen below...hm...!
+}
+
+VsVars
+
+Write-host "VsVars"
+Write-host " - Added VsVars to powershell env"
